ff2a207f2cee57d3439dc2ea796b4e5c8f8cf63b
abletoburntheweb <abtw324@gmail.com>
2024-01-20 18:02:14 +0300

    "add an exit option"

--- bj.py
+++ bj.py
@@ -9,38 +9,41 @@ def play_game():
     bot_score = 0
 
     while player_score <= 21 and bot_score <= 21:
-        # Ход игрока
-        input("Нажмите Enter, чтобы взять карту...")
-        print("=====================================")
-        player_card = deal_card()
-        print(f"Вы взяли {player_card}.")
-        player_score += player_card
-        print("=====================================")
-        input("Нажмите Enter, чтобы бот взял карту...")
-        print("=====================================")
-        bot_card = deal_card()
-        print(f"Бот взял {bot_card}.")
-        bot_score += bot_card
-        print("=====================================")
-        print(f"Ваш текущий счёт: {player_score}.")
-        print(f"Счёт бота: {bot_score}.")
-        print("=====================================")
 
-    if player_score == bot_score:
-        print("Ничья!")
-    elif bot_score == 0:
-        print("Вы проиграли! У дилера Blackjack!")
-    elif player_score == 0:
-        print("Поздравляю! У вас Blackjack!")
-    elif player_score > 21:
-        print("Вы проиграли! Перебор!")
-    elif bot_score > 21: 
-        print("Поздравляю! Дилер перебрал, вы выиграли!")
-    elif player_score > bot_score:
-        print("Поздравляю! Вы победили!")
-    elif player_score and bot_score > 21:
-        print("Ничья!")
+        start = input("Нажмите Enter, чтобы взять карту...")
+        while start == "":
+            print("=====================================")
+            player_card = deal_card()
+            print(f"Вы взяли {player_card}.")
+            player_score += player_card
+            print("=====================================")
+            input("Нажмите Enter, чтобы бот взял карту...")
+            print("=====================================")
+            bot_card = deal_card()
+            print(f"Бот взял {bot_card}.")
+            bot_score += bot_card
+            print("=====================================")
+            print(f"Ваш текущий счёт: {player_score}.")
+            print(f"Счёт бота: {bot_score}.")
+            print("=====================================")
+            start == 1
+            if player_score == bot_score:
+                start = input("Ничья!")
+            elif bot_score == 0:
+                start = input("Вы проиграли! У дилера Blackjack!")
+            elif player_score == 0:
+                start = input("Поздравляю! У вас Blackjack!")
+            elif player_score > 21:
+                start = input("Вы проиграли! Перебор!")
+            elif bot_score > 21:
+                start = input("Поздравляю! Дилер перебрал, вы выиграли!")
+            elif player_score > bot_score:
+                start = input("Поздравляю! Вы победили!")
+            elif player_score and bot_score > 21:
+                start = input("Ничья!")
+            else:
+                start = input("Вы проиграли!")
     else:
-        print("Вы проиграли!")
+        input("Прощайте!")
 
 play_game()
